aea96a5ac23402f9ab68a0bdadaa3babd94bfb8f
Larry Kenyon <larry@dufflite.com>
2019-08-25 08:38:12 -0700

    Update the rest of the headers to standard Tidepool format.

--- TidepoolKit/Types/TPUser/TPUserProfile.swift
+++ TidepoolKit/Types/TPUser/TPUserProfile.swift
@@ -1,17 +1,10 @@
-/*
- * Copyright (c) 2019, Tidepool Project
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the associated License, which is identical to the BSD 2-Clause
- * License as published by the Open Source Initiative at opensource.org.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the License for more details.
- *
- * You should have received a copy of the License along with this program; if
- * not, you can obtain one from Tidepool Project at tidepool.org.
- */
+//
+//  TPUserProfile.swift
+//  TidepoolKit
+//
+//  Created by Larry Kenyon on 8/23/19.
+//  Copyright © 2019 Tidepool Project. All rights reserved.
+//
 
 import Foundation
 
