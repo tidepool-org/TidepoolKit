058d8a980dcbc7609f607e4f6e3ffb7b733aeb3c
Rick Pasetto <rickpasetto@yahoo.com>
2021-09-10 15:04:13 -0700

    PR Feedback

--- TidepoolKitTests/Model/TInfoTests.swift
+++ TidepoolKitTests/Model/TInfoTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import TidepoolKit
 
 class TInfoTests: XCTestCase {
-    let info = TInfo(versions: TInfo.TVersionInfo(loop: TInfo.TVersionInfo.TLoopVersionInfo(minimumSupported: "1.2.0", criticalUpdateNeeded: ["1.1.0"])))
+    let info = TInfo(versions: TInfo.Versions(loop: TInfo.Versions.Loop(minimumSupported: "1.2.0", criticalUpdateNeeded: ["1.1.0"])))
     let infoJSONDictionary: [String: Any] = [
         "versions": [
             "loop": [
@@ -22,19 +22,5 @@ class TInfoTests: XCTestCase {
     
     func testCodableAsJSON() {
         XCTAssertCodableAsJSON(info, infoJSONDictionary)
-    }
-    func testNeedsCriticalUpdate() {
-        XCTAssertFalse(info.versions.loop!.needsCriticalUpdate(version: "1.2.0"))
-        XCTAssertTrue(info.versions.loop!.needsCriticalUpdate(version: "1.1.0"))
-    }
-    func testNeedsSupportedUpdate() {
-        XCTAssertFalse(info.versions.loop!.needsSupportedUpdate(version: "1.2.0"))
-        XCTAssertFalse(info.versions.loop!.needsSupportedUpdate(version: "1.2.1"))
-        XCTAssertFalse(info.versions.loop!.needsSupportedUpdate(version: "2.1.0"))
-        XCTAssertTrue(info.versions.loop!.needsSupportedUpdate(version: "0.1.0"))
-        XCTAssertTrue(info.versions.loop!.needsSupportedUpdate(version: "0.3.0"))
-        XCTAssertTrue(info.versions.loop!.needsSupportedUpdate(version: "0.3.1"))
-        XCTAssertTrue(info.versions.loop!.needsSupportedUpdate(version: "1.1.0"))
-        XCTAssertTrue(info.versions.loop!.needsSupportedUpdate(version: "1.1.99"))
     }
 }
