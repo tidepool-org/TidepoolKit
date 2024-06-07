5c92b4ffc29cabf07609b367e3f82d0f733a6bc1
Pete Schwamb <pete@schwamb.net>
2024-06-07 16:25:36 -0500

    LOOP-1169 Device log uploads (#71)
    * Adding types and methods for uploading device logs
    
    * Adding method to fetch device log metadata
    
    * Add new files to xcodeproj
    
    * Updates from pr review

---
+++ Sources/TidepoolKit/Model/TDeviceLogsMetadata.swift
@@ -0,0 +1,22 @@
+//
+//  TDeviceLogsMetadata.swift
+//  TidepoolKit
+//
+//  Created by Pete Schwamb on 6/3/24.
+//  Copyright © 2019 Tidepool Project. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+public struct TDeviceLogsMetadata: Codable, Equatable {
+    public var id: String
+    public var userId: String
+    public var digestMD5: String
+    public var mediaType: String
+    public var size: Int
+    public var createdTime: Date
+    public var startAtTime: Date
+    public var endAtTime: Date
+}
